d1d8fd4f1932e9e3d5b800592fcf08e4f94c7e9d
teymuruae <baratov.teymur@yandex.ru>
2026-04-30 12:42:07 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,17 +7,10 @@ from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password='str',
-    last_name='str',
-    first_name='str',
-    middle_name='str'
-)
+create_user_request = CreateUserRequestSchema()
 
 create_user_response = public_users_client.create_user(create_user_request)
 
@@ -26,20 +19,12 @@ files_client = get_files_client(auth_data)
 course_client = get_courses_client(auth_data)
 exercise_client = get_exercises_client(auth_data)
 
-create_file_request = CreateFileRequestSchema(
-    filename='cat.jpg',
-    directory='files',
-    upload_file='./testdata/files/cat.jpg')
+create_file_request = CreateFileRequestSchema(upload_file='./testdata/files/cat.jpg')
 
 create_file_response = files_client.create_file(create_file_request)
 print(f"Create file data: {create_file_response}")
 
 create_course_request = CreateCourseRequestSchema(
-    title="Math",
-    max_score=100,
-    min_score=10,
-    description="Math course",
-    estimated_time='1 month',
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
 )
@@ -47,13 +32,7 @@ create_course_response = course_client.create_course(create_course_request)
 print(f'Create course data: {create_course_response}')
 
 create_exercise_request = CreateExerciseRequestSchema(
-    title="abs",
     course_id=create_course_response.course.id,
-    max_score=100,
-    min_score=10,
-    order_index=12121,
-    description="some descr",
-    estimated_time="1 month"
 )
 
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
