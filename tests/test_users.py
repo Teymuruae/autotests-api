3025209f059d15315dce1935225b0a99ae4db6b1
teymuruae <baratov.teymur@yandex.ru>
2026-04-30 17:04:44 +0300

    assertions

---
+++ tests/test_users.py
@@ -0,0 +1,21 @@
+import http
+
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.assertions.base import assert_status_code
+from tools.assertions.users import assert_create_user_response
+
+
+def test_create_user():
+    public_users_client = get_public_users_client()
+    request = CreateUserRequestSchema()
+
+    create_user_response = public_users_client.create_user_api(request)
+    create_user_response_data = CreateUserResponseSchema.model_validate_json(create_user_response.text)
+
+    assert_status_code(create_user_response.status_code, http.HTTPStatus.OK)
+    assert_create_user_response(request, create_user_response_data)
+    validate_json_schema(create_user_response.json(), CreateUserResponseSchema.model_json_schema())
+
+
