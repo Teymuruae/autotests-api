756cab624b72ebe3a8adef86a5cc18b1c0da1fba
teymuruae <baratov.teymur@yandex.ru>
2026-05-05 20:19:27 +0300

    config

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -5,11 +5,14 @@ from pydantic import BaseModel
 
 from clients.auth.auth_api_client import get_authentication_client
 from clients.auth.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
+from config import settings
 
 
-class AuthenticationUserSchema(BaseModel, frozen = True):
+class AuthenticationUserSchema(BaseModel, frozen=True):
     email: str
     password: str
+
 
 @lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
@@ -23,4 +26,9 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     login_response = auth_api_client.login(LoginRequestSchema(email=user.email, password=user.password))
 
     headers = {"Authorization": f"Bearer {login_response.token.access_token}"}
-    return Client(timeout=100, base_url='http://localhost:8000', headers=headers)
+    return Client(
+        timeout=settings.http_client.timeout,
+        base_url=settings.http_client.client_url,
+        headers=headers,
+        event_hooks={'request': [curl_event_hook]}
+    )
